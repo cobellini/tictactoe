8f7f53f8bf6f342d7a0c90ec1deb7136a5973bbd
unknown <cobellini@gmail.com>
2019-02-13 18:56:39 +0000

    built board.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+const enum Player { 
+  None = 0,
+  One = 1,
+  Two = 2
+}
 
-class App extends React.Component {
+// interface IState {
+//   board: Player[];
+// }
+
+class App extends React.Component{
+
+public state = {
+  board: [Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None],
+ 
+}
+
+
+public renderCell = (index: number) => {
+  return <div className="cell" />
+}
+
+public renderBoard = () => {
+  const {board} = this.state;
+  return <div className="BoardContainer">{board.map((value, key) => this.renderCell(key))}</div>
+}
+
+
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Hello</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          <div className="BoardContainer">
+            {this.renderBoard()}
+          </div>
       </div>
     );
   }
