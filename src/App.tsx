bee287a36e6770f34a400ade115ac630e0573fbd
unknown <cobellini@gmail.com>
2019-02-17 00:20:06 +0000

    created onclick event to edit state and determine who's turn it was and changed bground color of div accordingly..

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import './App.css';
 
+// Enum to define which player is currently clicking on what tile
 const enum Player { 
   None = 0,
   One = 1,
   Two = 2
 }
 
+// Interface to contract state properties.
 interface IState{ 
-  board: Player[]
+  board: Player[],
+  playerTurn: Player
 }
 
+// Properties Interface to contract props items.
 interface IProps{
 
 }
@@ -18,19 +22,34 @@ interface IProps{
 
 class App extends React.Component<IProps, IState>{
 
+// Define state and initialize the game board as an array of Player enums in a 3x3 grid [9 squares]
+// create an property to store the turn of the player that is calculated based on onclick event handler
  public state = {
   board: [Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None],
- 
+  playerTurn: Player.One
 }
 
-CreateonclickHandler = (index: number) => (event: any) =>{
-  console.log("hello world" + index); 
+// function that is called during onClick event to create an event handler to edit the state of the application.
+// destructures the state object into seperate variables
+// returns the entire board array into a new constant board variable 
+// assigns the new player enum value to the array index passed by the handler
+// updates state of the new board array values and the players turn
+CreateOnclickHandler = (index: number) => (event: any) =>{
+  const {board, playerTurn} = this.state;
+  const updatedBoard = board.slice();
+  updatedBoard[index] = playerTurn;
+  this.setState({ board: updatedBoard, playerTurn: 3 - playerTurn}) 
 }
 
- _renderCell = (index: number, ) => {
-  return <div className="cell" onClick={this.CreateonclickHandler(index)} />
+// Renders a cell to represent each index of the board array, receives & passes the current index to allow the onclick handler 
+// to alter the board arrays value at supplied index. (creates all 9(0-8) onclick event listeners and passes in the value of that cell tldr.)
+ _renderCell = (index: number) => {
+   const {board} = this.state
+  return <div className="cell" onClick={this.CreateOnclickHandler(index)} data-player={board[index]}  />
 }
  
+// destructures the board property from the state object and iterates through the array, calling renderCell on each index, which returns the div cell with an eventlistener 
+// to edit the array value based on the passed in index integer, supplied by the mapping function key parameter.
  _renderBoard = () => {
   const {board} = this.state;
   return <div className="BoardContainer">{board.map((value, key) => this._renderCell(key))}</div>
